974187fdb16500a1a40bd54c27acea387cf53371
Sergey Shalagin <maloi2000_1@mail.ru>
2022-12-04 00:10:40 +0300

    Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.

---
+++ Temp1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.Clear();
+Console.Write("Введите число  = ");
+int num = Math.Abs(int.Parse(Console.ReadLine()!));
+int numTemp = num;
+int count = 0;
+
+while (numTemp > 0)
+{
+    numTemp /= 10;
+    count++;
+}
+
+if (count - 3 >= 0)
+    while (count -3 > 0)
+    {
+        num = num / 10;
+        count--;
+    }
+Console.WriteLine($"Третья цифра {num % 10}");
