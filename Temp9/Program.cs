e1f8b6905d1ccd04d16e8db60786d5f3ce3d0299
Sergey Shalagin <maloi2000_1@mail.ru>
2022-12-11 16:48:30 +0300

    Задача 29

---
+++ Temp9/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33] 
+
+Console.Clear();
+Console.Write("Введите количество элементов массива:\t");
+int PrintRandArray = int.Parse(Console.ReadLine()!);
+int[] myArray = new int[PrintRandArray];
+int[] GetNewArr(int[] myArray)
+{
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        Console.Write($"\nВедите элемент массива под индексом {i}:\t ");
+        myArray[i] = int.Parse(Console.ReadLine()!);
+    }
+    Console.WriteLine("Выод массива:");
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        Console.Write(myArray[i] + ", ");
+    }
+    return myArray;
+}
+GetNewArr(myArray);
