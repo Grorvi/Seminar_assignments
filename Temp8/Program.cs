286fb3e44a78fff178a5f6bc2f4cb5dbb7f20997
Sergey Shalagin <maloi2000_1@mail.ru>
2022-12-11 16:45:44 +0300

    Задача 27

---
+++ Temp8/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int i = int.Parse(Console.ReadLine()!);
+int GetSumNums(int number)
+{
+    int sum = 0;
+    while (i > 0)
+    {
+        int num = i % 10;
+        i = i / 10;
+        sum = sum + num;
+    }
+    return sum;
+}
+Console.WriteLine($"Сумма цифр = {GetSumNums(i)}");
